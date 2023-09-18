003c461b4ef3b318e6b95b980f0f929169e3d33a
Petar Panayotov <dakona98@gmail.com>
2023-09-18 19:29:07 +0300

    Task card seperated from container

---
+++ src/app/components/task-card/task-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { TodoService } from 'src/app/services/todo-service.service';
+import { Category } from 'src/app/types/columnList';
+
+@Component({
+  selector: 'app-task-card',
+  templateUrl: './task-card.component.html',
+  styleUrls: ['./task-card.component.sass']
+})
+export class TaskCardComponent {
+  @Input() data: Category | undefined;
+
+  constructor(public toDoService:TodoService) {}
+
+  onDragged = this.toDoService.onDragged
+  onDrop = this.toDoService.onDrop
+}
