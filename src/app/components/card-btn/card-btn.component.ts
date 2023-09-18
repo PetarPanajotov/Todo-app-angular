34052da3d424ccbea113c391d6ebf717940abed8
Petar Panayotov <dakona98@gmail.com>
2023-09-18 19:29:51 +0300

    Card btn component

---
+++ src/app/components/card-btn/card-btn.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-btn',
+  templateUrl: './card-btn.component.html',
+  styleUrls: ['./card-btn.component.sass']
+})
+export class CardBtnComponent {
+
+}
