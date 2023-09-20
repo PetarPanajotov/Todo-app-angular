e157f53c1d0b79cc93e535005fd295854df3ec4e
Petar Panayotov <dakona98@gmail.com>
2023-09-20 20:22:16 +0300

    Update nav.component.ts

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TodoService } from 'src/app/services/todo-service.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,7 @@ export class NavComponent {
   constructor (private toDoService: TodoService) {}
 
   onClick() {
-    const newList = { title: 'New list', draggableItem: [] };
+    const newList = { id: uuidv4(), title: 'New list', draggableItem: [] };
     this.toDoService.setColumnData(newList)
   }
 }
