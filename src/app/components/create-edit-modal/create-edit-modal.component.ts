00da0ea25d6298bad6a5c2551a2abc36f375e212
Petar Panayotov <dakona98@gmail.com>
2023-09-20 18:12:32 +0300

    Implement modal

--- src/app/components/create-edit-modal/create-edit-modal.component.ts
+++ src/app/components/create-edit-modal/create-edit-modal.component.ts
@@ -1,18 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ModalService } from 'src/app/services/modal.service';
+import { TodoService } from 'src/app/services/todo-service.service';
+import { Category, TaskDraggable, Task } from 'src/app/types/task-management.models';
 
 @Component({
   selector: 'app-create-edit-modal',
   templateUrl: './create-edit-modal.component.html',
   styleUrls: ['./create-edit-modal.component.sass']
 })
-export class CreateEditModalComponent {
+export class CreateEditModalComponent implements OnInit, OnDestroy {
   isEdit: boolean = false;
+  title = this.isEdit ? 'Edit Task' : 'Create Task';
+  columnId = this.modalService.columnId;
+  columnData!: Category[];
   tags: string[] = [];
-  tag: string = ''
-  title = this.isEdit? 'Edit Task': 'Create Task'
-  constructor(public modalService: ModalService) {}
-  onTagsChange(tag: string): void {
+  tag: string = '';
+
+  taskCardForm = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl(''),
+    dueTo: new FormControl(''),
+  });
+
+  ngOnInit(): void {
+    this.todoService.columnData.subscribe((data: any) => {
+      this.columnData = data
+    })
+  }
+
+  constructor(public modalService: ModalService, private todoService: TodoService) { }
+
+  onTagsChange(event: Event, tag: string): void {
     this.tags.push(tag)
+  };
+
+  onSubmit(): void {
+    const column = this.columnData.find(column => column.id === this.columnId)
+    const { name, description, dueTo } = this.taskCardForm.value;
+    if (name && description && dueTo) {
+      const contentItem: Task = {
+        name: name,
+        description: description,
+        dueTo: dueTo,
+        tags: this.tags
+      }
+      const formData: TaskDraggable = {
+        content: contentItem
+      }
+      column?.draggableItem.push(formData)
+    }
+    console.log(column)
+  }
+  ngOnDestroy(): void {
   }
 };
