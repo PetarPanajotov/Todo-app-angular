341bef0357e5cdbefba18f6ece6b094d872750b1
Petar Panayotov <dakona98@gmail.com>
2023-09-20 18:12:02 +0300

    fixed imports

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TodoService } from 'src/app/services/todo-service.service';
-import { Category } from 'src/app/types/columnList';
+import { Category } from 'src/app/types/task-management.models';
 
 @Component({
   selector: 'app-task-card',
